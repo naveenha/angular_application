375cd7c1592e9bdd73f60fda6934e443c2c9a98c
Naveen Kumar H A <naveen.ha@ellucian.com>
2021-03-12 16:40:56 +0530

    Updating code

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,5 +1,5 @@
-export const API_URL_LOCAL = "http://localhost:8080"
-export const API_JPA_URL_LOCAL = "http://localhost:8080/jpa"
+export const API_URL = "http://localhost:8080"
+export const API_JPA_URL = "http://localhost:8080/jpa"
 
-export const API_JPA_URL = "http://34.121.28.53:8080/jpa"
-export const API_URL = "http://34.121.28.53:8080"
+// export const API_URL = "http://34.121.28.53:8080"
+// export const API_JPA_URL = "http://34.121.28.53:8080/jpa"
